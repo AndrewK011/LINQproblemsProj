75c2afb4185daf657cab38d9b1eb32e6bcb06a00
Andrew K <drummer4life93@yahoo.com>
2020-06-09 11:19:21 -0500

    Added method to take in a list of strings and return an IEnumerable string minus duplicate strings.

--- LINQproblems/Program.cs
+++ LINQproblems/Program.cs
@@ -11,10 +11,15 @@ namespace LINQproblems
         static void Main(string[] args)
         {
             var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
             Strings s = new Strings();
 
             words = s.GetWordsWithTH(words);
             s.PrintNewList(words);
+
+            var result = s.ReturnListMinusDuplicateStrings(names);
+            s.PrintNewList(result);
+
         }
     }
 }
--- LINQproblems/Strings.cs
+++ LINQproblems/Strings.cs
@@ -22,13 +22,20 @@ namespace LINQproblems
             return newList;
         }
 
-        public void PrintNewList(List<string> stringToPrint)
+        public void PrintNewList(IEnumerable<string> stringToPrint)
         {
             foreach (string word in stringToPrint)
             {
                 Console.WriteLine(word);
             }
             Console.ReadLine();
+        }
+
+        public IEnumerable<string> ReturnListMinusDuplicateStrings(List<string> listParam)
+        {
+            IEnumerable<string> enumerable = listParam.Distinct();
+
+            return enumerable;
         }
     }
 }
